cae9d3f94c1f006a2f66ad49a0259c38ff61c61e
delaying <pgy5638@naver.com>
2023-03-02 16:20:48 +0900

    rncli Calculator android native function 작성

--- RNCalculator/android/app/src/main/java/com/rncalculator/CalculatorModule.kt
+++ RNCalculator/android/app/src/main/java/com/rncalculator/CalculatorModule.kt
@@ -1,5 +1,35 @@
 package com.rncalculator
 
-class CalculatorModule {
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
 
+class CalculatorModule(reactContext:ReactApplicationContext):ReactContextBaseJavaModule(reactContext) {
+    override fun getName() = "CalculatorModule"
+
+    @ReactMethod
+    fun executeCalc(action:String, numberA:Double, numberB:Double, promise: Promise){
+        if(action === "plus"){
+            promise.resolve(numberA + numberB);
+            return;
+        }
+
+        if(action === "minus"){
+            promise.resolve(numberA - numberB);
+            return;
+        }
+
+        if(action === "multiply"){
+            promise.resolve(numberA * numberB);
+            return;
+        }
+
+        if(action === "devide"){
+            promise.resolve(numberA/numberB);
+            return;
+        }
+
+        promise.reject("Unexpected action type")
+    }
 }
--- RNCalculator/android/app/src/main/java/com/rncalculator/MainApplication.java
+++ RNCalculator/android/app/src/main/java/com/rncalculator/MainApplication.java
@@ -1,6 +1,7 @@
 package com.rncalculator;
 
 import android.app.Application;
+import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+            packages.add(new CalculatorPackage());
+
           return packages;
         }
 
