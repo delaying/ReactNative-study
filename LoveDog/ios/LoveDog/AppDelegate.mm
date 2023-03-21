7b5d79831b00bbfaae3faf31b9610964113f8f4a
delaying <pgy5638@naver.com>
2023-03-21 16:58:03 +0900

    LoveDog firebase 설정

--- LoveDog/ios/LoveDog/AppDelegate.mm
+++ LoveDog/ios/LoveDog/AppDelegate.mm
@@ -1,4 +1,6 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
+
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -6,10 +8,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+
   self.moduleName = @"LoveDog";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
