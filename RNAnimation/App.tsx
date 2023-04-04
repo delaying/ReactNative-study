047de7ae57bfb1d86274450b8cddd5e92952e0ff
delaying <pgy5638@naver.com>
2023-04-04 22:54:56 +0900

    skeleton, snow background 실습

--- RNAnimation/App.tsx
+++ RNAnimation/App.tsx
@@ -11,12 +11,15 @@ import Snackbar from './src/chap3/Snackbar';
 import DrawerMenu from './src/chap3/DrawerMenu';
 import Collapse from './src/chap3/Collapse';
 import Progressbar from './src/chap3/Progressbar';
+import Skeleton from './src/chap3/Skeleton';
+import SnowAnimation from './src/chap3/SnowAnimation';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Progressbar />
-    </SafeAreaView>
+    // <SafeAreaView style={{flex: 1}}>
+    //   <Skeleton />
+    // </SafeAreaView>
+    <SnowAnimation />
   );
 }
 
