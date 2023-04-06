dba9631e414da099f7cdfe9d32570dea40f80555
delaying <pgy5638@naver.com>
2023-04-06 22:54:38 +0900

    PanResponder font slider

--- RNAnimation/App.tsx
+++ RNAnimation/App.tsx
@@ -9,9 +9,10 @@ import React from 'react';
 import PanresponderBall from './src/chap6/PanresponderBall';
 import PanresponderModal from './src/chap6/PanresponderModal';
 import PanresponderBanner from './src/chap6/PanresponderBanner';
+import PanresponderFont from './src/chap6/PanresponderFont';
 
 function App(): JSX.Element {
-  return <PanresponderBanner />;
+  return <PanresponderFont />;
 }
 
 export default App;
